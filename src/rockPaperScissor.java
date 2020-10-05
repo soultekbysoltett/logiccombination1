09cefe078a58cf853949580459893292dc539cf6
soultekbysoltett <soultekbysoltett@gmail.com>
2020-10-05 15:38:55 -0400

    Use of && and ||

--- src/rockPaperScissor.java
+++ src/rockPaperScissor.java
@@ -8,10 +8,33 @@ public class rockPaperScissor {
         System.out.println("Player 2: Rock | Paper | Scissor | ");
         String playerTwo = input.next();
 
+        if ( (playerOne.equals("rock") && playerTwo.equals("scissors")) || (playerOne.equals("scissors") &&
+                playerTwo.equals("paper")) || (playerOne.equals("paper") && playerTwo.equals("rock"))) {
+            System.out.println("Player 1 Wins");
+        } else {
+            System.out.println("Player 2 Wins");
+        }
+
+    }
+}
+        /*
         if (playerOne.equals("rock")) {
             if (playerTwo.equals("scissors")) {
                 System.out.println("Player 1 Wins");
-            } else
+            } else if (playerTwo.equals("paper")) {
+                System.out.println("Player 2 Wins");
+            } else {
+                System.out.println("Tie");
+            }
+        } else if (playerOne.equals("paper")) {
+            if (playerTwo.equals("scissors")) {
+                System.out.println("Player 2 Wins");
+            } else if (playerTwo.equals("paper")) {
+                System.out.println("Tie");
+            } else { // playerTwo.equals("rock")
+                System.out.println("Player 1 Wins");
+            }
         }
     }
 }
+*/
