f6a99277a413daf5f2b9615187152c578510d08a
soultekbysoltett <soultekbysoltett@gmail.com>
2020-10-05 13:41:28 -0400

    1st if statement added

--- src/rockPaperScissor.java
+++ src/rockPaperScissor.java
@@ -1,4 +1,17 @@
-
+import java.util.Scanner;
 
 public class rockPaperScissor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Player 1: Rock | Paper | Scissor | ");
+        String playerOne = input.next();
+        System.out.println("Player 2: Rock | Paper | Scissor | ");
+        String playerTwo = input.next();
+
+        if (playerOne.equals("rock")) {
+            if (playerTwo.equals("scissors")) {
+                System.out.println("Player 1 Wins");
+            } else
+        }
+    }
 }
