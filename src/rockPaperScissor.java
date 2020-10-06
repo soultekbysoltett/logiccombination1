9d09b5373acf7de9ee6bec3aacea88a7d9c49be9
soultekbysoltett <soultekbysoltett@gmail.com>
2020-10-06 10:40:11 -0400

    Added Java Class poker

--- src/rockPaperScissor.java
+++ src/rockPaperScissor.java
@@ -1,32 +1,42 @@
 import java.util.Scanner;
 
 public class rockPaperScissor {
+
     public static void main(String[] args) {
-        combiningConditions();
+
+        combiningConditions1();
     }
 
-    public static void combiningConditions () {
+    public static void combiningConditions1 () {
 
-            Scanner input = new Scanner(System.in);
-            System.out.println("Player 1: Rock | Paper | Scissors ");
-            String playerOne = input.next();
-            System.out.println("Player 2: Rock | Paper | Scissors ");
-            String playerTwo = input.next();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Player 1: Rock | Paper | Scissors ");
+        String playerOne = input.next();
+        System.out.println("Player 2: Rock | Paper | Scissors ");
+        String playerTwo = input.next();
 
-            if ((playerOne.equals("Rock") && playerTwo.equals("Scissors")) || (playerOne.equals("Scissors") &&
-                    playerTwo.equals("Paper")) || (playerOne.equals("Paper") && playerTwo.equals("Rock"))) {
-                System.out.println("Player 1 Wins");
-            } else if ((playerOne.equals("Rock") && playerTwo.equals("Rock")) || (playerOne.equals("Scissors") &&
-                    playerTwo.equals("Scissors")) || (playerOne.equals("Paper") && playerTwo.equals("Paper"))) {
-                System.out.println("Tie");
-            } else {
-                System.out.println("Player 2 Wins");
-            }
-
+        // Rock, Paper, Scissors using Operators through combining Boolean Expressions.
+        if ((playerOne.equals("Rock") && playerTwo.equals("Scissors")) || (playerOne.equals("Scissors") &&
+                playerTwo.equals("Paper")) || (playerOne.equals("Paper") && playerTwo.equals("Rock"))) {
+            System.out.println("Player 1 Wins");
+        } else if ((playerOne.equals("Rock") && playerTwo.equals("Rock")) || (playerOne.equals("Scissors") &&
+                playerTwo.equals("Scissors")) || (playerOne.equals("Paper") && playerTwo.equals("Paper"))) {
+            System.out.println("Tie");
+        } else {
+            System.out.println("Player 2 Wins");
         }
 
     }
-        /*
+
+    public static void combiningConditions2 () {
+
+        // (Rock, Paper, Scissor using nested if statements)
+        Scanner input = new Scanner(System.in);
+        System.out.println("Player 1: Rock | Paper | Scissors ");
+        String playerOne = input.next();
+        System.out.println("Player 2: Rock | Paper | Scissors ");
+        String playerTwo = input.next();
+
         if (playerOne.equals("rock")) {
             if (playerTwo.equals("scissors")) {
                 System.out.println("Player 1 Wins");
@@ -44,6 +54,9 @@ public class rockPaperScissor {
                 System.out.println("Player 1 Wins");
             }
         }
-    }
+
+
+
+
+    d}
 }
-*/
