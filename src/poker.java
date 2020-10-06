54bd4c7280da2a223243cd5aaa64ab501f72f62b
soultekbysoltett <soultekbysoltett@gmail.com>
2020-10-06 12:55:17 -0400

    Added new class daysinMonth

--- src/poker.java
+++ src/poker.java
@@ -1,2 +1,49 @@
+import java.util.Scanner;
+
 public class poker {
-}
+
+    public static void main(String[] args) {
+
+        poker();
+
+    }
+
+    // Combining conditions
+    public static void poker () {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Player1's Hand");
+        String hand = input.next();
+
+        if (hand.equals("sraight")) {
+            if (hand.equals("flush")) {
+                System.out.println("straight flush");
+            } else {
+                System.out.println("straight");
+            }
+        } else if (hand.equals("flush")) {
+            System.out.println("flush");
+        }
+
+    }
+
+    // using boolean expressions for simplification
+    public static void pokerTwo() {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Player1's Hand");
+        String hand = input.nextLine();
+
+        if (hand.equals("straight && flush")) {
+            System.out.println("straight flush");
+        } else if (hand.equals("straight")) {
+            System.out.println("straight");
+        } else if (hand.equals("flush")) {
+            System.out.println("flush");
+        }
+
+    }
+
+    }
