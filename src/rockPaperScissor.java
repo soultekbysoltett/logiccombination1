8b12072d8df2d747d08ea5906a6c5f5b811fe31d
soultekbysoltett <soultekbysoltett@gmail.com>
2020-10-06 09:05:20 -0400

    Added Code is there is a Tie

--- src/rockPaperScissor.java
+++ src/rockPaperScissor.java
@@ -2,21 +2,30 @@ import java.util.Scanner;
 
 public class rockPaperScissor {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Player 1: Rock | Paper | Scissor | ");
-        String playerOne = input.next();
-        System.out.println("Player 2: Rock | Paper | Scissor | ");
-        String playerTwo = input.next();
+        combiningConditions();
+    }
 
-        if ( (playerOne.equals("rock") && playerTwo.equals("scissors")) || (playerOne.equals("scissors") &&
-                playerTwo.equals("paper")) || (playerOne.equals("paper") && playerTwo.equals("rock"))) {
-            System.out.println("Player 1 Wins");
-        } else {
-            System.out.println("Player 2 Wins");
+    public static void combiningConditions () {
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("Player 1: Rock | Paper | Scissors ");
+            String playerOne = input.next();
+            System.out.println("Player 2: Rock | Paper | Scissors ");
+            String playerTwo = input.next();
+
+            if ((playerOne.equals("Rock") && playerTwo.equals("Scissors")) || (playerOne.equals("Scissors") &&
+                    playerTwo.equals("Paper")) || (playerOne.equals("Paper") && playerTwo.equals("Rock"))) {
+                System.out.println("Player 1 Wins");
+            } else if ((playerOne.equals("Rock") && playerTwo.equals("Rock")) || (playerOne.equals("Scissors") &&
+                    playerTwo.equals("Scissors")) || (playerOne.equals("Paper") && playerTwo.equals("Paper"))) {
+                System.out.println("Tie");
+            } else {
+                System.out.println("Player 2 Wins");
+            }
+
         }
 
     }
-}
         /*
         if (playerOne.equals("rock")) {
             if (playerTwo.equals("scissors")) {
