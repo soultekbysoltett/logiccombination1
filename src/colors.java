b7e96eeed54983f6074927c25867e3242ae4a1b5
soultekbysoltett <soultekbysoltett@gmail.com>
2020-10-06 15:00:27 -0400

    Finished colors code 2

--- src/colors.java
+++ src/colors.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class colors {
+
+
+
+    public static void main (String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("What color do you want? ");
+        String color = input.next();
+
+        if (color.equals("R") || color.equals("r")) {
+            System.out.println("You have chosen Red");
+        } else if (color.equals("G") || color.equals("g")) {
+            System.out.println("You have chosen Green");
+        } else if (color.equals("B") || color.equals("b")) {
+            System.out.println("You have chosen Blue");
+        } else {
+            System.out.println("Unknown color: " + color);
+        }
+
+    }
 }
