e2a21c90e5a7b49abde6d863d98441e98b1b6627
soultekbysoltett <soultekbysoltett@gmail.com>
2020-10-11 14:29:28 -0400

    added loops

--- src/loops.java
+++ src/loops.java
@@ -1,2 +1,13 @@
+
+
 public class loops {
+    public static void main(String[] args) {
+        //for (int i = 0; i <= 10; i++) {
+        //for (int j = 0; j <= 6; j += 2) {
+        for (int x = 4; x > 0; x--) {
+            //System.out.println("The current value of i: " + i);
+            //System.out.println("The current value of j: " + j);
+            //System.out.println("The current value of x: " + x);
+        }
+    }
 }
